c55c226844a1159ccd33ac2a11c1a44456b44ce9
Raufoon <minhaz.raufoon.1567@gmail.com>
2020-04-07 17:26:43 +0200

    Replace project image slider with React Hooks

--- src/App/routes/Projects/components/ProjectCard/components/ImageSlider/index.js
+++ src/App/routes/Projects/components/ProjectCard/components/ImageSlider/index.js
@@ -1,43 +1,38 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './style.css'
 
-class ImageSlider extends React.PureComponent {
-  state = {
-    currentIdx: 0
-  }
+const ImageSlider = props => {
+  const {className, images} = props;
 
-  componentDidMount() {
-    setTimeout(() => this.sliderInterval = requestAnimationFrame(this.slide), 5000)
-  }
-  componentWillUnmount() {
-    cancelAnimationFrame(this.sliderInterval);
-  }
+  const [currentIdx, setCurrentIndex] = useState(0);
 
-  slide = () => {
-    this.setState(prevState => ({
-      currentIdx: (prevState.currentIdx + 1) % this.props.images.length
-    }), function callBack(){
-      setTimeout(() => this.sliderInterval = requestAnimationFrame(this.slide), 5000);
-    })
-  }
+  const slide = () => setCurrentIndex(prev => (prev + 1) % images.length);
 
-  render() {
-    const {currentIdx} = this.state;
-    const {className, images} = this.props;
-    return (
-      <div className={`ImageSlider ${className}`}>
-        {
-          images.map((image, idx) => (
-            <div className="slidableImage"
-              key={image}
-              onMouseOver={undefined}
-              onMouseOut={undefined}
-              style={{backgroundImage: `url(${image})`, left: `${(idx - currentIdx)*100}%`}}/>
-          ))
-        }
-      </div>
-    )
-  }
+  useEffect(function slideEffect() {
+    let timeout;
+    let animFrame = requestAnimationFrame(function delayedSlide() {
+      timeout = setTimeout(slide, 5000)
+    });
+
+    return function clear() {
+      clearTimeout(timeout);
+      cancelAnimationFrame(animFrame);
+    }
+  });
+
+  return (
+    <div className={`ImageSlider ${className}`}>
+      {
+        images.map((image, idx) => (
+          <div className="slidableImage"
+            key={image}
+            onMouseOver={undefined}
+            onMouseOut={undefined}
+            style={{backgroundImage: `url(${image})`, left: `${(idx - currentIdx)*100}%`}}/>
+        ))
+      }
+    </div>
+  )
 }
 
 export default ImageSlider

d0f853c9d43664b0439dfd2a04274db5c40f77f7
Raufoon <minhaz.raufoon.1567@gmail.com>
2020-12-13 21:43:21 +0100

    Add navbar toggle button for mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,32 @@
+import { useCallback, useRef, useState } from "react"
 import { NavLink } from "react-router-dom"
+import { ReactComponent as ExpandIcon } from "../assets/icons/arrow-down.svg"
+import { ReactComponent as ShrinkIcon } from "../assets/icons/arrow-up.svg"
 import "./Header.css"
 
 export default function Header() {
+  const navBarRef = useRef(null)
+  const [isMobileNavVisible, setMobileNavVisible] = useState(false)
+
+  const toggleMobileNav = useCallback(() => {
+    setMobileNavVisible((prev) => {
+      if (prev) {
+        navBarRef.current.style.display = "none"
+      } else {
+        navBarRef.current.style.display = "flex"
+      }
+      return !prev
+    })
+  }, [])
+
   return (
     <header className="Header">
       <h1>Minhaz Raufoon</h1>
-      <nav>
+      <button className="navCollapseButton" onClick={toggleMobileNav}>
+        {isMobileNavVisible ? <ShrinkIcon /> : <ExpandIcon />}
+      </button>
+
+      <nav ref={navBarRef}>
         <NavLink exact to="/">
           Me
         </NavLink>

c5f99ab2c742e80e469c3ea5c90527045abb35a2
Raufoon <minhaz.raufoon.1567@gmail.com>
2020-12-17 02:13:32 +0100

    Fix template

--- src/components/project/FullStackTemplate.js
+++ src/components/project/FullStackTemplate.js
@@ -1,5 +1,6 @@
 import PhotoSlider from "../PhotoSlider"
 import styles from "./FullStackTemplate.module.css"
+import ProjectLinkIcon from "./ProjectLinkIcon"
 
 export default function FullStackTemplate(props) {
   const {
@@ -18,25 +19,32 @@ export default function FullStackTemplate(props) {
       <div className={styles.monolith}>
         <h3>{type}</h3>
       </div>
+
       <div className={styles.details}>
         <h1>{title}</h1>
         <p>{subtitle}</p>
+
         <div className={styles.technologies}>
           {technologies.map((tech) => (
             <label key={tech}>#{tech}</label>
           ))}
         </div>
+
         <div className={styles.links}>
           {links.map((link) => (
-            <span>{link.type}</span>
+            <a key={link.type} href={link.url} target="_blank" rel="noreferrer">
+              <ProjectLinkIcon type={link.type} />
+            </a>
           ))}
         </div>
       </div>
+
       <PhotoSlider
         className={styles.desktopImageSlider}
         photos={desktopImages}
         frameColor={"#065"}
       />
+
       <PhotoSlider
         frameColor={"#669"}
         className={styles.mobileImageSlider}
---
+++ src/components/project/ProjectLinkIcon.js
@@ -0,0 +1,16 @@
+import { ReactComponent as GithubIcon } from "./assets/icons/github.svg"
+import { ReactComponent as DownloadIcon } from "./assets/icons/download3.svg"
+import { ReactComponent as WebsiteIcon } from "./assets/icons/earth.svg"
+
+export default function ProjectLinkIcon({ type }) {
+  switch (type) {
+    case "github":
+      return <GithubIcon />
+    case "website":
+      return <WebsiteIcon />
+    case "download":
+      return <DownloadIcon />
+    default:
+      throw new Error("Unsupported project type")
+  }
+}

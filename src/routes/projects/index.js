63df6172a972d3b806adf863cd2c40652a0ce350
Raufoon <minhaz.raufoon.1567@gmail.com>
2020-12-17 16:47:17 +0100

    Sort projects

--- src/routes/projects/index.js
+++ src/routes/projects/index.js
@@ -1,3 +1,4 @@
+import { useMemo } from "react"
 import Loader from "../../components/Loader"
 import Project from "../../components/Project"
 import useFetchListFromDB from "../../useFetchListFromDB"
@@ -6,11 +7,16 @@ import styles from "./index.module.css"
 export default function Projects() {
   const { isFetching, list } = useFetchListFromDB(`myprojects`)
 
+  const sortedProjectList = useMemo(() => {
+    if (!list) return []
+    return list.sort((a, b) => (a.priority > b.priority ? -1 : 1))
+  }, [list])
+
   if (isFetching) return <Loader center={true} />
 
   return (
     <section className={styles.Projects}>
-      {list.map((project) => (
+      {sortedProjectList.map((project) => (
         <Project key={project.id} project={project} />
       ))}
     </section>

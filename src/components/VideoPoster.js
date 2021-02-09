ed6c07455022f471b015f7056f0dae24321b33e0
Raufoon <minhaz.raufoon.1567@gmail.com>
2021-02-09 16:51:44 +0100

    Add youtube video for projects

--- src/components/VideoPoster.js
+++ src/components/VideoPoster.js
@@ -1,9 +1,12 @@
 import styles from './VideoPoster.module.css'
 
 export default function VideoPoster({ className, src }) {
+  const isYoutubeLink = src.indexOf('youtube.com') !== -1
+
   return (
     <div className={`${styles.VideoPoster} ${className}`}>
-      <video src={src} controls></video>
+      {isYoutubeLink && <iframe title="Youtube link" src={src}></iframe>}
+      {!isYoutubeLink && <video src={src} controls></video>}
     </div>
   )
 }

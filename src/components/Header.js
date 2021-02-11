5642811e3b4e9e9cef9e1785f6dcd3d45213eaef
Raufoon <minhaz.raufoon.1567@gmail.com>
2021-02-11 23:10:43 +0100

    Add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -46,6 +46,12 @@ export default function Header() {
 
   return (
     <header className="Header">
+      <div className="logo">
+        <b>S C C</b>
+        <span>&nbsp;</span>
+        <span>&nbsp;</span>
+      </div>
+
       <Link className={'webtitle'} to="/">
         <h2>Minhaz Raufoon</h2>
       </Link>

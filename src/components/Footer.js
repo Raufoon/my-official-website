34641e89500b0a265d76726fae647f22661eb0f3
Raufoon <minhaz.raufoon.1567@gmail.com>
2021-02-16 00:25:11 +0100

    Style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,8 @@ import styles from './Footer.module.css'
 import { ReactComponent as GithubIcon } from '../assets/icons/github.svg'
 import { ReactComponent as LinkedinIcon } from '../assets/icons/linkedin2.svg'
 import { ReactComponent as XingIcon } from '../assets/icons/xing2.svg'
+import { ReactComponent as EmailIcon } from '../assets/icons/envelop.svg'
+import { ReactComponent as PhoneIcon } from '../assets/icons/phone.svg'
 
 export default function Footer() {
   const { isFetching, list } = useFetchListFromDB(`contact/socialLinks`)
@@ -21,8 +23,14 @@ export default function Footer() {
 
       <div className={styles.section}>
         <h3 className={styles.headline}>Contact</h3>
-        <label>minhaz.raufoon.1567@gmail.com</label>
-        <label>+491788680932</label>
+        <button>
+          <EmailIcon />
+          &nbsp;&nbsp;minhaz.raufoon.1567@gmail.com
+        </button>
+        <button>
+          <PhoneIcon />
+          &nbsp;&nbsp;+491788680932
+        </button>
       </div>
 
       <div className={styles.section}>

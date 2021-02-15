d6ff27f318003dc05d8b49c549e9dc26e3c18da5
Raufoon <minhaz.raufoon.1567@gmail.com>
2021-02-15 22:36:03 +0100

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,62 @@
-import styles from "./Footer.module.css"
+import { Link } from 'react-router-dom'
+import useFetchListFromDB from '../useFetchListFromDB'
+import styles from './Footer.module.css'
+import { ReactComponent as GithubIcon } from '../assets/icons/github.svg'
+import { ReactComponent as LinkedinIcon } from '../assets/icons/linkedin2.svg'
+import { ReactComponent as XingIcon } from '../assets/icons/xing2.svg'
 
 export default function Footer() {
+  const { isFetching, list } = useFetchListFromDB(`contact/socialLinks`)
+
+  if (isFetching) return false
+
   return (
     <footer className={styles.Footer}>
+      <div className={styles.section}>
+        <h3 className={styles.headline}>Sitemap</h3>
+        <Link to="/">About me</Link>
+        <Link to="/projects">My projects</Link>
+        <Link to="/resume">My resume</Link>
+      </div>
+
       <div className={styles.section}>
         <h3 className={styles.headline}>Contact</h3>
         <label>minhaz.raufoon.1567@gmail.com</label>
         <label>+491788680932</label>
       </div>
+
+      <div className={styles.section}>
+        <h3 className={styles.headline}>Follow</h3>
+        <div className={styles.socialLinks}>
+          {list.map((socialLink) => {
+            const Icon = getIconComponentBySocialLink(socialLink.type)
+            return (
+              <a
+                key={socialLink.type}
+                href={socialLink.url}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Icon />
+                &nbsp;&nbsp;{socialLink.type}
+              </a>
+            )
+          })}
+        </div>
+      </div>
     </footer>
   )
+}
+
+function getIconComponentBySocialLink(linkType) {
+  switch (linkType) {
+    case 'github':
+      return GithubIcon
+    case 'linkedin':
+      return LinkedinIcon
+    case 'xing':
+      return XingIcon
+    default:
+      return null
+  }
 }
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -2,9 +2,6 @@ import Poster from '../../components/Poster'
 import styles from './index.module.css'
 import useFetchFromDB from '../../useFetchFromDB'
 import useFetchListFromDB from '../../useFetchListFromDB'
-import { ReactComponent as GithubIcon } from '../../assets/icons/github.svg'
-import { ReactComponent as LinkedinIcon } from '../../assets/icons/linkedin2.svg'
-import { ReactComponent as XingIcon } from '../../assets/icons/xing2.svg'
 import { useContext } from 'react'
 import { SettingsContext } from '../../settings'
 import Loader from '../../components/Loader'
@@ -13,42 +10,21 @@ export default function Home() {
   const { settings } = useContext(SettingsContext)
   const aboutMe = useFetchFromDB(`${settings.lang}/me`)
   const photosData = useFetchListFromDB(`photos`)
-  const socialLinksData = useFetchListFromDB(`contact/socialLinks`)
 
-  if (aboutMe.isFetching || photosData.isFetching || socialLinksData.isFetching)
-    return <Loader />
+  if (aboutMe.isFetching || photosData.isFetching) return <Loader />
 
   const { subtitle, about } = aboutMe
 
   const photos = photosData.list
   const randomizedPhoto = photos[Math.floor(Math.random() * photos.length)]
 
-  const socialLinks = socialLinksData.list
-
   return (
     <section className={styles.Home}>
       <div className={styles.photoContainer}>
         <Poster className={styles.poster} src={randomizedPhoto} />
-
-        <div className={styles.socialLinks}>
-          {socialLinks.map((link) => {
-            const Icon = getIconComponentBySocialLink(link.type)
-            return (
-              <a
-                key={link.type}
-                href={link.url}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <Icon />
-              </a>
-            )
-          })}
-        </div>
       </div>
 
       <div className={styles.aboutMe}>
-        {/**<PosterQuote /> */}
         <h1>{subtitle}</h1>
         {about.map((para, index) => (
           <p key={index} dangerouslySetInnerHTML={{ __html: para }}></p>
@@ -56,17 +32,4 @@ export default function Home() {
       </div>
     </section>
   )
-}
-
-function getIconComponentBySocialLink(linkType) {
-  switch (linkType) {
-    case 'github':
-      return GithubIcon
-    case 'linkedin':
-      return LinkedinIcon
-    case 'xing':
-      return XingIcon
-    default:
-      return null
-  }
 }

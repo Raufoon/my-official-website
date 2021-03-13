314e92de1258391b3abfed0c2e976009ac093d49
Raufoon <minhaz.raufoon.1567@gmail.com>
2021-03-13 16:03:56 +0100

    Refactor project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,12 +1,7 @@
-import { useContext, useMemo } from 'react'
-import { Link } from 'react-router-dom'
+import { useContext } from 'react'
 import { SettingsContext } from '../settings'
 import useFetchFromDB from '../useFetchFromDB'
-import PhotoSlider from './PhotoSlider'
-import styles from './Project.module.css'
-import ProjectLinkIcon from './ProjectLinkIcon'
-import TechLabel from './TechLabel'
-import VideoPoster from './VideoPoster'
+import ProjectCard from './ProjectCard'
 
 export default function Project({ project, invertLayout }) {
   const { id, type, photos, technologies, links, video } = project
@@ -17,75 +12,19 @@ export default function Project({ project, invertLayout }) {
     `${settings.lang}/projects/${id}`
   )
 
-  const monolithStyle = useMemo(
-    () => ({
-      backgroundColor: getMonolithTheme(type),
-    }),
-    [type]
-  )
-
   if (isFetching || hasError) return false
 
   return (
-    <div className={`${styles.Project} ${invertLayout ? styles.inverted : ''}`}>
-      <div className={styles.monolith} style={monolithStyle}>
-        <h3>{type}</h3>
-      </div>
-
-      <div className={styles.details}>
-        <h1>{title}</h1>
-        <p>{subtitle}</p>
-        <div className={styles.technologies}>
-          {technologies.map((tech) => (
-            <TechLabel key={tech} type={tech} />
-          ))}
-        </div>
-        <div className={styles.links}>
-          {links.map((link) => (
-            <a key={link.type} href={link.url} target="_blank" rel="noreferrer">
-              <ProjectLinkIcon type={link.type} />
-              &nbsp;&nbsp;{link.type}
-            </a>
-          ))}
-
-          <Link to={`/projects/${id}`}>more</Link>
-        </div>
-      </div>
-
-      {video ? (
-        <VideoPoster className={styles.photoSlider} src={video} />
-      ) : (
-        <PhotoSlider
-          className={styles.photoSlider}
-          frameColor={'lightgray'}
-          photos={photos}
-        />
-      )}
-    </div>
+    <ProjectCard
+      id={id}
+      type={type}
+      photos={photos || []}
+      technologies={technologies || []}
+      links={links || []}
+      video={video}
+      title={title}
+      subtitle={subtitle}
+      invertLayout={invertLayout}
+    />
   )
-}
-
-function getMonolithTheme(projectType) {
-  switch (projectType) {
-    case 'Frontend Web':
-      return '#3b9ed6'
-
-    case 'Desktop App':
-      return '#cc6540'
-
-    case 'Desktop Game':
-      return '#f6546a'
-
-    case 'Full Stack Web':
-      return '#6f549a'
-
-    case 'Assembly':
-      return '#ff6367'
-
-    case 'Research Project':
-      return '#003366'
-
-    default:
-      return 'var(--color-2)'
-  }
 }
---
+++ src/components/ProjectCard.js
@@ -0,0 +1,91 @@
+import { useMemo } from 'react'
+import { Link } from 'react-router-dom'
+import PhotoSlider from './PhotoSlider'
+import styles from './ProjectCard.module.css'
+import ProjectLinkIcon from './ProjectLinkIcon'
+import TechLabel from './TechLabel'
+import VideoPoster from './VideoPoster'
+
+export default function ProjectCard(props) {
+  const {
+    id,
+    title,
+    subtitle,
+    type,
+    invertLayout,
+    photos,
+    technologies,
+    links,
+    video,
+  } = props
+
+  const monolithStyle = useMemo(
+    () => ({
+      backgroundColor: getMonolithTheme(type),
+    }),
+    [type]
+  )
+
+  return (
+    <div className={`${styles.Project} ${invertLayout ? styles.inverted : ''}`}>
+      <div className={styles.monolith} style={monolithStyle}>
+        <h3>{type}</h3>
+      </div>
+
+      <div className={styles.details}>
+        <h1>{title}</h1>
+        <p>{subtitle}</p>
+        <div className={styles.technologies}>
+          {technologies.map((tech) => (
+            <TechLabel key={tech} type={tech} />
+          ))}
+        </div>
+        <div className={styles.links}>
+          {links.map((link) => (
+            <a key={link.type} href={link.url} target="_blank" rel="noreferrer">
+              <ProjectLinkIcon type={link.type} />
+              &nbsp;&nbsp;{link.type}
+            </a>
+          ))}
+
+          <Link to={`/projects/${id}`}>more</Link>
+        </div>
+      </div>
+
+      {video ? (
+        <VideoPoster className={styles.photoSlider} src={video} />
+      ) : (
+        <PhotoSlider
+          className={styles.photoSlider}
+          frameColor={'lightgray'}
+          photos={photos}
+        />
+      )}
+    </div>
+  )
+}
+
+function getMonolithTheme(projectType) {
+  switch (projectType) {
+    case 'Frontend Web':
+      return '#3b9ed6'
+
+    case 'Desktop App':
+      return '#cc6540'
+
+    case 'Desktop Game':
+      return '#f6546a'
+
+    case 'Full Stack Web':
+      return '#6f549a'
+
+    case 'Assembly':
+      return '#ff6367'
+
+    case 'Research Project':
+      return '#003366'
+
+    default:
+      return 'var(--color-2)'
+  }
+}

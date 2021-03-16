256449677d3d902059ef75aed2404b7f39a9fffd
Raufoon <minhaz.raufoon.1567@gmail.com>
2021-03-16 23:44:44 +0100

    Style update

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -5,31 +5,36 @@ import useFetchListFromDB from '../../useFetchListFromDB'
 import { useContext } from 'react'
 import { SettingsContext } from '../../settings'
 import Loader from '../../components/Loader'
+import APODSection from './APODSection'
 
 export default function Home() {
   const { settings } = useContext(SettingsContext)
-  const aboutMe = useFetchFromDB(`${settings.lang}/me`)
+  const { subtitle, about, isFetching: isAboutFetching } = useFetchFromDB(
+    `${settings.lang}/me`
+  )
   const photosData = useFetchListFromDB(`photos`)
 
-  if (aboutMe.isFetching || photosData.isFetching) return <Loader />
-
-  const { subtitle, about } = aboutMe
+  if (isAboutFetching || photosData.isFetching) return <Loader />
 
   const photos = photosData.list
   const randomizedPhoto = photos[Math.floor(Math.random() * photos.length)]
 
   return (
-    <section className={styles.Home}>
-      <div className={styles.photoContainer}>
-        <Poster className={styles.poster} src={randomizedPhoto} />
-      </div>
+    <div className={styles.Home}>
+      <section className={styles.intro}>
+        <div className={styles.photoContainer}>
+          <Poster className={styles.poster} src={randomizedPhoto} />
+        </div>
 
-      <div className={styles.aboutMe}>
-        <h1>{subtitle}</h1>
-        {about.map((para, index) => (
-          <p key={index} dangerouslySetInnerHTML={{ __html: para }}></p>
-        ))}
-      </div>
-    </section>
+        <div className={styles.aboutMe}>
+          <h1>{subtitle}</h1>
+          {about.map((para, index) => (
+            <p key={index} dangerouslySetInnerHTML={{ __html: para }}></p>
+          ))}
+        </div>
+      </section>
+
+      <APODSection />
+    </div>
   )
 }

68f112390dc3c37b6a7b5e93fb5d6d7c51b303c1
Raufoon <minhaz.raufoon.1567@gmail.com>
2021-05-30 17:09:16 +0200

    Remove inline styles

--- src/components/ThemePicker/index.tsx
+++ src/components/ThemePicker/index.tsx
@@ -1,28 +1,20 @@
-import { toggleTheme } from '../../utils'
-import style from './index.module.css'
-import { ReactComponent as MoonIcon } from './moon-fill.svg'
-import { ReactComponent as SunIcon } from './sun.svg'
-import { AppTheme } from '../../global-types'
+import { toggleTheme } from "../../utils"
+import style from "./index.module.css"
+import { ReactComponent as MoonIcon } from "./moon-fill.svg"
+import { ReactComponent as SunIcon } from "./sun.svg"
+import { AppTheme } from "../../global-types"
 
 interface Props {
-  currentTheme: AppTheme;
+  currentTheme: AppTheme
 }
 
 export default function ThemePicker(props: Props) {
-  const isDark = props.currentTheme === 'dark'
+  const isDark = props.currentTheme === "dark"
   const Icon = isDark ? MoonIcon : SunIcon
 
   return (
     <button className={style.ThemePicker} onClick={toggleTheme}>
-      <Icon
-        style={{
-          left: isDark ? 0 : 'unset',
-          right: isDark ? 'unset' : 0,
-          transform: `translateY(-50%) translateX(${isDark ? '-50%' : '50%'})`,
-          backgroundColor: isDark ? 'white' : '#ffb42b',
-          fill: isDark ? 'gray' : 'black',
-        }}
-      />
+      <Icon />
     </button>
   )
 }

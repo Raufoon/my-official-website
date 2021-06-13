5eb0e6fa156e7692b299ffacb8d2c835ba485672
Raufoon <minhaz.raufoon.1567@gmail.com>
2021-06-13 23:56:00 +0200

    Refactor

--- src/components/project/ProjectLinkIcon.tsx
+++ src/components/project/ProjectLinkIcon.tsx
@@ -1,8 +1,8 @@
-import { ReactComponent as GithubIcon } from '../../assets/icons/github.svg'
-import { ReactComponent as DownloadIcon } from '../../assets/icons/download3.svg'
-import { ReactComponent as WebsiteIcon } from '../../assets/icons/earth.svg'
-import { ReactComponent as DocumentIcon } from '../../assets/icons/attachment.svg'
-import { ReactComponent as DockerIcon } from '../../assets/icons/docker.svg'
+import { ReactComponent as GithubIcon } from "../../assets/icons/github.svg"
+import { ReactComponent as DownloadIcon } from "../../assets/icons/download3.svg"
+import { ReactComponent as WebsiteIcon } from "../../assets/icons/earth.svg"
+import { ReactComponent as DocumentIcon } from "../../assets/icons/attachment.svg"
+import { ReactComponent as DockerIcon } from "../../assets/icons/docker.svg"
 
 interface Props {
   type: string
@@ -10,17 +10,19 @@ interface Props {
 
 export default function ProjectLinkIcon({ type }: Props) {
   switch (type) {
-    case 'github':
+    case "github":
       return <GithubIcon />
-    case 'website':
+    case "website":
       return <WebsiteIcon />
-    case 'download':
+    case "download":
       return <DownloadIcon />
-    case 'document':
+    case "download (linux)":
+      return <DownloadIcon />
+    case "document":
       return <DocumentIcon />
-    case 'docker':
+    case "docker":
       return <DockerIcon />
     default:
-      throw new Error('Unsupported project link type')
+      throw new Error("Unsupported project link type")
   }
 }

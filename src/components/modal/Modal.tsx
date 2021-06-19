f2bb4505f2a95574156997808dbda852838fb2fd
Raufoon <minhaz.raufoon.1567@gmail.com>
2021-06-19 21:28:28 +0200

    Close modal on location change

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -3,6 +3,7 @@ import IconButton from "../IconButton"
 import { closeLastModal } from "./utils"
 import { ReactComponent as CloseIcon } from "./close-icon.svg"
 import { ModalSettings } from "./types"
+import { useEffect } from "react"
 
 interface Props {
   zIndex: number
@@ -12,6 +13,18 @@ interface Props {
 
 export default function Modal(props: Props) {
   const { zIndex, settings, children } = props
+
+  useEffect(function registerLocationChangeListeners() {
+    function closeLastModalOnLocationChange() {
+      closeLastModal()
+    }
+
+    window.addEventListener("popstate", closeLastModalOnLocationChange)
+
+    return () => {
+      window.removeEventListener("popstate", closeLastModalOnLocationChange)
+    }
+  }, [])
 
   return (
     <section className={styles.Modal} style={{ zIndex }}>
